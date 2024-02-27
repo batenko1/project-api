04a7dd1bb3f3780ff0654efe64b7b4d5501b05ce
vladbatenko <v_batenko@viyar.ua>
2024-02-27 14:13:58 +0200

    add tesseract

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -1,5 +1,77 @@
 @extends('layout')
 
 @section('content')
+    @extends('layout')
+
+    @section('css')
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/css/rtl/core.css') }}"
+              class="template-customizer-core-css">
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/css/rtl/theme-default.css') }}"
+              class="template-customizer-theme-css">
+    @endsection
+
+    @section('content')
+        <div class="content-wrapper">
+            <!-- Content -->
+
+            <div class="container-xxl flex-grow-1 container-p-y">
+                <h4 class=""><span class="text-muted fw-light">Покупатели</h4>
+
+
+                <!-- Basic Bootstrap Table -->
+                <div class="card">
+                    <div class="table-responsive text-nowrap">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>#id</th>
+                                <th>Имя</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody class="table-border-bottom-0">
+                            @foreach($accounts as $account)
+                                <tr>
+                                    <td>
+                                        <span class="fw-medium">{{ $account->id }}</span>
+                                    </td>
+                                    <td>{{ $account->fio }}</td>
+
+                                    <td>
+                                        <div class="dropdown">
+                                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                                <i class="ti ti-dots-vertical"></i>
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                @can('edit account')
+                                                    <a class="dropdown-item" href="{{ route('admin.accounts.edit', $user->id) }}">
+                                                        <i class="ti ti-pencil me-1"></i> Редактировать</a>
+                                                @endcan
+
+                                                @can('delete account')
+                                                    <a class="dropdown-item" href="{{ route('admin.accounts.destroy', $user->id) }}">
+                                                        <i class="ti ti-trash me-1"></i> Удалить</a>
+                                                @endcan
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <!--/ Responsive Table -->
+            </div>
+            <!-- / Content -->
+
+            <!-- / Footer -->
+
+            <div class="content-backdrop fade"></div>
+        </div>
+    @endsection
 
 @endsection
---
+++ resources/views/settings/create.blade.php
@@ -0,0 +1,67 @@
+@extends('layout')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}" />
+@endsection
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content -->
+
+        <div class="container-xxl flex-grow-1 container-p-y">
+            <h4 class="py-3 mb-4"><span class="text-muted fw-light">Настройки /</span> Создать</h4>
+
+            <!-- Basic Layout & Basic with Icons -->
+            <div class="row">
+                <!-- Basic Layout -->
+                <div class="col-xxl">
+                    <div class="card mb-4">
+
+                        <div class="card-body">
+                            <form method="post" action="{{ route('admin.settings.store') }}">
+                                @csrf
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label" for="basic-default-name">Имя</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="basic-default-name" value="{{ old('name') }}" name="name"/>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label" for="basic-default-name">Почта</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="basic-default-name" value="{{ old('email') }}" name="email" />
+                                    </div>
+                                </div>
+
+
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label" for="basic-default-name">Пароль</label>
+                                    <div class="col-sm-10">
+                                        <input type="password" class="form-control" id="basic-default-name" name="password" />
+                                    </div>
+                                </div>
+
+
+                                <div class="row justify-content-end">
+                                    <div class="col-sm-10">
+                                        <button type="submit" class="btn btn-primary">Создать</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- / Footer -->
+
+        <div class="content-backdrop fade"></div>
+    </div>
+@endsection
+
+@section('js')
+    <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}"></script>
+    <script src="{{ asset('assets/js/form-layouts.js') }}"></script>
+@endsection
