ea770d4c9cf4a63a4e6be3d85555b65276b77fbe
johny <batenko4@gmail.com>
2024-03-01 08:44:48 +0200

    fix bugs

--- app/Http/Controllers/ChangeStatusOrderController.php
+++ app/Http/Controllers/ChangeStatusOrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ChangeStatusOrderController extends Controller
 {
@@ -13,6 +14,29 @@ class ChangeStatusOrderController extends Controller
         $order = Order::query()->findOrFail($request->order_id);
         $order->status = $request->status;
         $order->save();
+
+        if($order->status == 'cancel') {
+
+            if($order->bonuses) {
+
+                $user = DB::connection('mysql_bonuses')
+                    ->table('users')
+                    ->where('identification_code', $order->account->identification_code)
+                    ->first();
+
+                if($user) {
+                    DB::connection('mysql_bonuses')
+                        ->table('bonuses')
+                        ->insert([
+                            'user_id' => $user->id,
+                            'bonuses' => $order->bonuses,
+                            'type' => 'add'
+                        ]);
+                }
+
+            }
+
+        }
 
         return response()->json($order, 201);
 
